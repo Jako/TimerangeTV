8612a2233f86ba69a294f7138b28c084d4e3d85d
Thomas Jakobi <thomas.jakobi@partout.info>
2021-03-22 10:18:40 +0100

    Fix swapped lexicon entries. Solves #4

--- core/components/timerangetv/lexicon/de/tvrenders.inc.php
+++ core/components/timerangetv/lexicon/de/tvrenders.inc.php
@@ -13,9 +13,9 @@ $_lang['timerangetv.from'] = "Von";
 $_lang['timerangetv.to'] = "Bis";
 
 $_lang['timerangetv.max_value'] = 'Maximalwert';
-$_lang['timerangetv.max_value_desc'] = 'Der Mindestwert der Zeiteingabe. Der Standardwert ist 00:00.';
+$_lang['timerangetv.max_value_desc'] = 'Der Maximalwert der Zeiteingabe. Der Standardwert ist 23:59.';
 $_lang['timerangetv.min_value'] = 'Minimalwert';
-$_lang['timerangetv.min_value_desc'] = 'Der Maximalwert der Zeiteingabe. Der Standardwert ist 23:59.';
+$_lang['timerangetv.min_value_desc'] = 'Der Mindestwert der Zeiteingabe. Der Standardwert ist 00:00.';
 $_lang['timerangetv.time_format'] = "Zeitformat";
 $_lang['timerangetv.time_format_desc'] = "Das Format muss nach <a href=\"http://docs.sencha.com/ext-js/3-4/#!/api/Date-static-method-parseDate\" target=\"_blank\">Date.parseDate</a> gültig sein (Standard: Manager-Zeitformat).";
 $_lang['timerangetv.time_increment'] = "Zeitabstand";
--- core/components/timerangetv/lexicon/en/tvrenders.inc.php
+++ core/components/timerangetv/lexicon/en/tvrenders.inc.php
@@ -13,9 +13,9 @@ $_lang['timerangetv.from'] = "From time";
 $_lang['timerangetv.to'] = "To time";
 
 $_lang['timerangetv.max_value'] = 'Maximum value';
-$_lang['timerangetv.max_value_desc'] = 'The minimum value of the time inputs. Defaults to 00:00.';
+$_lang['timerangetv.max_value_desc'] = 'The maximum value of the time inputs. Defaults to 00:00.';
 $_lang['timerangetv.min_value'] = 'Minimum value';
-$_lang['timerangetv.min_value_desc'] = 'The maximum value of the time inputs. Defaults to 23:59.';
+$_lang['timerangetv.min_value_desc'] = 'The minimum value of the time inputs. Defaults to 23:59.';
 $_lang['timerangetv.time_format'] = "Time format";
 $_lang['timerangetv.time_format_desc'] = "The format must be valid according to <a href=\"http://docs.sencha.com/ext-js/3-4/#!/api/Date-static-method-parseDate\" target=\"_blank\">Date.parseDate</a> (defaults to manager date format).";
 $_lang['timerangetv.time_increment'] = "Time increment";
