6a3f4555951636ee7abc75855c8dcc81f1a41e67
Thomas Jakobi <buero@treehillstudio.de>
2021-05-18 09:41:31 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: MODX Extras/MODX TimerangeTV/Standard
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/modx-timerangetv-standard/nl/

--- core/components/timerangetv/lexicon/nl/default.inc.php
+++ core/components/timerangetv/lexicon/nl/default.inc.php
@@ -6,16 +6,13 @@
  * @subpackage language
  */
 $_lang['timerangetv'] = "TimerangeTV";
-
 $_lang['timerange'] = "Tijdreeks (van <> tot)";
-
 $_lang['timerangetv.from'] = "Van tijd";
 $_lang['timerangetv.to'] = "Tot tijd";
-
 $_lang['timerangetv.max_value'] = 'Maximale waarde';
-$_lang['timerangetv.max_value_desc'] = 'De maximale waarde van de tijdinvoer. Standaard ingesteld op 23:59. ';
+$_lang['timerangetv.max_value_desc'] = 'De maximale waarde van de tijdinvoer. Standaard ingesteld op 23:59.';
 $_lang['timerangetv.min_value'] = 'Minimale waarde';
-$_lang['timerangetv.min_value_desc'] = 'De minimumwaarde van de tijdinvoer. Standaard ingesteld op 00:00. ';
+$_lang['timerangetv.min_value_desc'] = 'De minimumwaarde van de tijdinvoer. Standaard ingesteld op 00:00.';
 $_lang['timerangetv.time_format'] = "Tijdsformaat";
 $_lang['timerangetv.time_format_desc'] = "Het formaat moet voldoen aan <a href=\"http://docs.sencha.com/ext-js/3-4/#!/api/Date-static-method-parseDate\" target=\"_blank\">Date.parseDate</a> (Standaard: Manager Datumformaat).";
 $_lang['timerangetv.time_increment'] = "Tijdsinterval";
