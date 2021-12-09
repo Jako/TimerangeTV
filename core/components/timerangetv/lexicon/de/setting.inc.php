37209b4eb3ece4f6d260a945f7b80086ae8767ab
Weblate (bot) <hosted@weblate.org>
2021-12-09 09:54:42 +0100

    Translations update from Hosted Weblate (#12)
    * Translated using Weblate (German)
    
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: MODX Extras/MODX TimerangeTV/System Setting
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/modx-timerangetv-system-setting/de/
    
    * Translated using Weblate (Dutch)
    
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: MODX Extras/MODX TimerangeTV/System Setting
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/modx-timerangetv-system-setting/nl/
    
    Co-authored-by: Thomas Jakobi <buero@treehillstudio.de>

--- core/components/timerangetv/lexicon/de/setting.inc.php
+++ core/components/timerangetv/lexicon/de/setting.inc.php
@@ -5,6 +5,5 @@
  * @package timerangetv
  * @subpackage lexicon
  */
-
 $_lang['setting_timerangetv.debug'] = 'Debug';
 $_lang['setting_timerangetv.debug_desc'] = 'Debug-Informationen im MODX Fehlerprotokoll ausgeben.';
