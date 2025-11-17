aee6694fd4654d0cac9b7ef8eff97b46d89df8bf
Thomas Jakobi <buero@treehillstudio.de>
2025-11-17 12:47:36 +0100

    Deleted translation using Weblate (Spanish)

--- core/components/timerangetv/lexicon/es/setting.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Setting Lexicon Entries for TimerangeTV
- *
- * @package timerangetv
- * @subpackage lexicon
- */
-$_lang['setting_timerangetv.debug'] = '';
-$_lang['setting_timerangetv.debug_desc'] = '';
